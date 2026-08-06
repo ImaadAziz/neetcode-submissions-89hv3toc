43c769758a0da9f625f232d6c3bb52a90079a60b
Imaad Aziz <57569767+ImaadAziz@users.noreply.github.com>
2026-08-06 15:39:48 -0500

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.py
@@ -0,0 +1,29 @@
+class MinStack:
+
+    def __init__(self):
+        self.mins = []
+        self.stack = []
+        
+
+    def push(self, val: int) -> None:
+        if len(self.mins) == 0:
+            self.mins.append(val)
+        elif val < self.mins[-1]:
+            self.mins.append(val)
+        else:
+            self.mins.append(self.mins[-1])
+        self.stack.append(val)
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.mins.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.mins[-1]
+        
